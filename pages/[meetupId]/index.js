15f14cb00f34f3ef4016e15a25c69f968ce9d0e9
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2022-01-16 14:57:27 +0530

    add server side props and static props

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,54 @@
-import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg"
-      title="Kremlin"
-      address="Some street, some city."
-      description="This is Moscow"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg",
+        title: "Kremlin",
+        address: "Some street, some city.",
+        description: "This is Moscow",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "Moscow",
-    image: "https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg",
+    image:
+      "https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg",
     address: "Red Square, Moscow.",
     description: "Kremlin",
   },
@@ -26,8 +27,31 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+
+// This is a function to call all the props before it is render, otherwise all the list wil not render for the first time.
+// This code will not run in the client side
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+  };
+}
+
+// Revalidate every incoming request
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
 
 export default HomePage;
