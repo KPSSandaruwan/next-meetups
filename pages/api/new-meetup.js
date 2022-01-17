45da745a640ae6158fe1477601e1a2aa02e2960b
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2022-01-17 10:21:17 +0530

    mongodb connection issue

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,15 +5,18 @@ async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const uri = environment.mongoDBConnection;
 
-    const client = await MongoClient.connect(environment.mongoDBConnection);
+    const client = await MongoClient.connect(uri);
+
+    console.log("client", client)
+
     const db = client.db();
 
     const meetupCollection = db.collection("meetups");
     const result = await meetupCollection.insertOne(data);
 
-    console.log(result);
+    console.log("result", result);
 
     client.close();
 
